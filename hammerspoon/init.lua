c652dd025f9e1d3f5758e065bf872351b52ec4fc
Aravind Dhanasekaran <hdnivara@gmail.com>
2017-04-21 10:24:55 -0700

    hs: Mac OS X style notification for 'Hello, world'

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,4 +6,8 @@ Hammerspoon configuration file
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
     -- Heads-up display notification
     hs.alert.show("Hello, Hammerspoon world!")
+
+    -- Mac OS X style notification
+    hs.notify.new({title="Hammerspoon",
+        informativeText="Hello, Hammerspoon world!"}):send()
 end)
